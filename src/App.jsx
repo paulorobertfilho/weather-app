b7ec4d4a136dd551104df19d7eb5198e9d8ac003
paulorobertfilho <paulorobert.filho@gmail.com>
2019-12-27 11:11:25 -0300

    add compnent+styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "./sass/app.scss";
 
+import TopSection from "./components/top/index";
+import BottomSection from "./components/bottom/index";
+
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <div className="main-container">Main Container</div>
+        <div className="main-container">
+          <div className="top-section">
+            <TopSection />
+          </div>
+          <div className="bottom-section">BOTTOM</div>
+        </div>
       </div>
     );
   }
---
+++ src/components/bottom/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.scss";
+
+export default class BottomSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>BottomSection</div>;
+  }
+}
